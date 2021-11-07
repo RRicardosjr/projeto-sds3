26ff8f45b52ac22381f2d8ee30ba2abe115c9251
Rubinei Silva <rricardosjr@gmail.com>
2021-11-07 11:36:34 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,6 @@
 
 import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { SaleSum } from "types/sale";
 import { BASE_URL } from "utils/request";
@@ -11,22 +12,29 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    let chartData: ChartData = { labels: [], series: [] };
+    
+    // Declaracao de chartData de forma sincrona
+    //let chartData: ChartData = { labels: [], series: [] };
+    // Carga de chartData de forma sincrona
+    //chartData = { labels: myLabels, series: mySeries };
 
-    axios.get(BASE_URL + '/sales/amount-by-seller')
-        .then((response)=> {
-            const data=response.data as SaleSum[];
-            const myLabels = data.map(x=>x.sellerName);
-            const mySeries = data.map(x=>x.sum);
+    // Carga considerando a renderização de tela e controle de estado
 
-            chartData = { labels: myLabels, series: mySeries };
-            console.log(chartData);
+    // Este comando mantem o estado da constante no processo assincrono
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-        });
-    // const mockData = {
-    //     series: [477138, 499928, 444867, 220426, 473088],
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
+    useEffect(() => {
+        axios.get(BASE_URL + '/sales/amount-by-seller')
+            .then((response) => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                // Carga de chartData de forma assincrona
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    },[]);
+
 
     const options = {
         legend: {
